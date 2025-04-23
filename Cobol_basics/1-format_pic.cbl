ba7df0f23a381560e101f28e3d675e4ef3dd7309
HamzaSD-DEV <saoudh20@gmail.com>
2025-04-23 12:43:55 +0100

    update task1

--- Cobol_basics/1-format_pic.cbl
+++ Cobol_basics/1-format_pic.cbl
@@ -8,15 +8,15 @@
        01 Account-Balance    PIC 9(5)V99  VALUE 1234.56.
        01 Interest-Rate      PIC 9V99     VALUE 5.75.
        
-       01 ACCOUNT-BALANCE-DISPLAY  PIC 9(5).99.
-       01 INTEREST-RATE-DISPLAY    PIC 9.99.
+       01 Account-Balance-DISPLAY  PIC 9(5).99.
+       01 Interest-Rate-DISPLAY    PIC 9.99.
 
        PROCEDURE DIVISION.
        BEGIN.
-           MOVE ACCOUNT-BALANCE TO ACCOUNT-BALANCE-DISPLAY
-           MOVE INTEREST-RATE TO INTEREST-RATE-DISPLAY
+           MOVE Account-Balance TO Account-Balance-DISPLAY
+           MOVE Interest-Rate TO Interest-Rate-DISPLAY
 
            DISPLAY "Customer ID : " Customer-ID
-           DISPLAY "Account Balance : " ACCOUNT-BALANCE-DISPLAY
-           DISPLAY "Interest Rate : " INTEREST-RATE-DISPLAY "%"
+           DISPLAY "Account Balance : " Account-Balance-DISPLAY
+           DISPLAY "Interest Rate : " Interest-Rate-DISPLAY "%"
            STOP RUN.
